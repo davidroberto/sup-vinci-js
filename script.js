c9f135860b5fbc661eeea1370ff921d4b137793b
David Robert <contact@david-robert.fr>
2024-03-07 15:39:11 +0100

    move game js

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const menuTriggerNode = document.querySelector(".menu-trigger");
+// Je créé un event listener, qui au se déclenche au moment ou
+// une touche du clavier est enfoncée
+// la variable event est fournie par le navigateur
+// elle contient des infos sur l'évènement (keydown) qui vient de se produire
+document.addEventListener("keydown", (event) => {
+  // la méthode preventDefault() de la variable event
+  // permet d'empêcher le comportement par défaut du navigateur pour l'évènement
+  event.preventDefault();
 
-menuTriggerNode.addEventListener("click", () => {
-  const menuNode = document.querySelector(".menu");
+  // je cherche dans le DOM un élément qui a pour class "avatar"
+  // et je stocke le résultat dans la variable avatarNode
+  const avatarNode = document.querySelector(".avatar");
 
-  if (menuNode.style.display === "none") {
-    menuNode.style.display = "block";
-    menuNode.style.opacity = 1;
-  } else {
-    menuNode.style.display = "none";
-    menuNode.style.opacity = 0;
+  // je récupère la position gauche de mon avatar
+  // si elle a pas été définie en CSS, je lui donne en valeur 0;
+  const left = avatarNode.style.left || 0;
+
+  // je récupère la position top de mon avatar
+  // si elle a pas été définie en CSS, je lui donne en valeur 0;
+  const top = avatarNode.style.top || 0;
+
+  // si la touche pressée est la gauche
+  if (event.key === "ArrowLeft") {
+    // je prends la position à gauche de l'avatar
+    // et je lui enlève 10 px
+    avatarNode.style.left = `${left - 10}px`;
+  } else if (event.key === "ArrowRight") {
+    avatarNode.style.left = `${left + 10}px`;
+  } else if (event.key === "ArrowDown") {
+    avatarNode.style.top = `${top + 10}px`;
+  } else if (event.key === "ArrowUp") {
+    avatarNode.style.top = `${top - 10}px`;
   }
 });
